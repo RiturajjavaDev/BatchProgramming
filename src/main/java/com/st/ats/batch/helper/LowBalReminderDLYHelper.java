60f1b7caadc9693c1c93d99e4c04965f7f6fc9b3
RiturajjavaDev <Rrituraj047@gmail.com>
2020-01-16 21:55:45 +0530

    Batch programing

---
+++ src/main/java/com/st/ats/batch/helper/LowBalReminderDLYHelper.java
@@ -0,0 +1,39 @@
+package com.st.ats.batch.helper;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.st.ats.batch.Constants.BatchConstants;
+import com.st.ats.mail.utils.LowBalMailUtils;
+import com.st.ats.reporitory.BatchRunDtlsRepo;
+
+@Service
+public class LowBalReminderDLYHelper {
+
+	@Autowired
+	private BatchRunDtlsRepo batchRunDtlRepo;
+	
+	@Autowired
+	private LowBalMailUtils balMailUtils;
+	
+	public void sendMessage(Long phno) {
+		try {
+			Thread.sleep(500);
+		}catch (Exception e) {
+		}
+	}
+
+	public void sendMail(String  email,String fname,String lname) {
+			//balMailUtils.sendMail(email, fname, lname);
+		try {
+			Thread.sleep(1000);
+		}catch (Exception e) {
+		}
+	}
+
+	public void updateBatchDtls(Long batchRunId) {
+		batchRunDtlRepo.updateBatchRunDtlsStatus("EN", new Date(), BatchConstants.LOW_BAL_BATCH_NAME, batchRunId);
+	}
+}
